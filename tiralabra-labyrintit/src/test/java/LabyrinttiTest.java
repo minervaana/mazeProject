4de4e376f17943a53287bc27aaeff60bcf9d7a21
karppienkingi <mirka.vaananen@gmail.com>
2021-09-17 13:50:27 +0300

    nopea päivitys testiin

--- tiralabra-labyrintit/src/test/java/LabyrinttiTest.java
+++ tiralabra-labyrintit/src/test/java/LabyrinttiTest.java
@@ -1,11 +1,18 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 
 /**
  *
  * @author Mirka
  */
+@RunWith(Suite.class)
+@SuiteClasses ({RuutuTest.class})
 public class LabyrinttiTest {
+    
     
     /**
      *
@@ -16,11 +23,14 @@ public class LabyrinttiTest {
     /**
      * Test of sideWinder method, of class Labyrintti.
      */
+    
     @Test
     public void testOnPolku() {
         System.out.println("sideWinder");
         Labyrintti l = new Labyrintti(10);
         l.sideWinder();
+        
+        Ruutu r = new Ruutu(0,0);
         
         
     }
